e5a496bab709807b7bc13f2d3f430fc414db476f
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-02-10 17:11:12 +0800

    update app

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 8080;
 
 app.use(
     require("./routes/404.js")
-)
+);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '../public/index.html')
+});
 
 app.listen(port, () => {
     console.log("Server is running~~\n\n\nhttp://localhost" + port);
