16c1480f1f8e1415c2eac70f8e5fbc0f04ea9a1e
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-02-10 17:58:59 +0800

    moar fixes!!!

--- routes/404.js
+++ routes/404.js
@@ -1,9 +1,10 @@
 module.exports = (function(){
     'use strict';
+    const path = require('path')
     const route = require('express').Router();
 
     route.get('*', (req, res) => {
-        res.sendFile(__dirname + '../public/index.html')
+        res.sendFile(path.resolve(__dirname + '/../public/index.html'))
     });
 
     return route
